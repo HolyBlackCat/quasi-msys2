7ab42bb38b58f89e6104cf35bc06842ed3d8df0f
HolyBlackCat <blckcat@inbox.ru>
2020-10-15 23:56:09 +0300

    A safer env variable setup script.

--- misc/env.sh
+++ misc/env.sh
@@ -1,11 +1,20 @@
-echo 'Make sure you run this script using `source ...`.'
+# Make a dummy function to be able to have local variables.
+__dummy_func() {
+    echo 'Make sure you run this script using `source ...`.'
 
-WINEPATH="$(realpath "$(dirname "$BASH_SOURCE")"/../root/mingw64/bin)"
-echo 'Your WINEPATH is now equal to `'"$WINEPATH"'`.'
-test -z "$WINEPATH" && return
-export WINEPATH
+    local new_winepath="$(realpath "$(dirname "$BASH_SOURCE")"/../root/mingw64/bin)"
+    # Make sure the resulting variable is not empty due to some error. If it is, abort.
+    test -z "$new_winepath" && return
+    export "WINEPATH=$new_winepath"
+    echo 'Your WINEPATH is now equal to `'"$WINEPATH"'`.'
 
-PATH="$(make -f "$(dirname "$BASH_SOURCE")/helpers/AddToPath.mk" "dir=$WINEPATH")"
-test -z "$PATH" && return
-export PATH
-echo 'Your PATH is now equal to `'"$PATH"'`.'
+    local new_path="$(make -f "$(dirname "$BASH_SOURCE")/helpers/AddToPath.mk" "dir=$WINEPATH")"
+    # Make sure the resulting variable is not empty due to some error. If it is, abort.
+    test -z "$new_path" && return
+    export "PATH=$new_path"
+    echo 'Your PATH is now equal to `'"$PATH"'`.'
+}
+# Call our dummy funcion.
+__dummy_func
+# Delete the function.
+unset -f __dummy_func

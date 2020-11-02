83a5db0b9b1a9da98a6b9c8837af59a25fc7fd49
HolyBlackCat <blckcat@inbox.ru>
2020-11-02 23:56:18 +0300

    `vars.src` now sets `ACLOCAL_PATH`.

--- env/internal/AddToPath.mk
+++ env/internal/AddToPath.mk
@@ -6,10 +6,13 @@
 # Some constants.
 override space := $(strip) $(strip)
 
+# Set this to the name of the variable you want to modify.
+var := PATH
+
 dirs = $(error Set `dirs` to the dirs you want to append to the PATH.)
 override dirs := $(subst :, ,$(subst $(space),<,$(dirs)))
 
-$(info $(subst <, ,$(subst $(space),:,$(strip $(dirs) $(filter-out $(dirs),$(subst :, ,$(subst $(space),<,$(PATH))))))))
+$(info $(subst <, ,$(subst $(space),:,$(strip $(dirs) $(filter-out $(dirs),$(subst :, ,$(subst $(space),<,$($(var)))))))))
 
 .PHONY: empty
 empty:

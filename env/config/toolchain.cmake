a3579d7e64760fe1951fc34a1bb2e0e548cd7151
HolyBlackCat <holyblckcat@gmail.com>
2025-03-30 00:52:07 +0500

    Append to `CMAKE_FIND_ROOT_PATH` instead of overwriting it to allow custom values.

--- env/config/toolchain.cmake
+++ env/config/toolchain.cmake
@@ -1,9 +1,12 @@
 # This file contains CMake configuration.
 
-SET(CMAKE_SYSTEM_NAME Windows)
-SET(CMAKE_CROSSCOMPILING FALSE)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_CROSSCOMPILING FALSE)
 
-SET(CMAKE_FIND_ROOT_PATH $ENV{MSYSTEM_PREFIX})
+# Note! Appending instead of overwriting, to allow the user to specify extra custom paths.
+# Because `CMAKE_FIND_ROOT_PATH` is really the only variable that lets us do it sanely.
+# Even Android NDK does this! https://github.com/android/ndk/issues/912
+list(APPEND CMAKE_FIND_ROOT_PATH "$ENV{MSYSTEM_PREFIX}")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
